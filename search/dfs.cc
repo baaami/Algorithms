ecf26cfde5a3ab8726747a0a11ed0cc7be85a5bc
baaami <lgh121546@naver.com>
2024-09-07 12:29:10 +0900

    깊이 우선 탐색 구현

---
+++ search/dfs.cc
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+#define MAX_NUM 7
+
+bool checked[MAX_NUM];
+vector<int> arr[MAX_NUM+1];
+
+void dfs(int x) {
+    if(checked[x]) return;
+    checked[x] = true;
+    cout << x << ' ';
+    
+    for(int i = 0; i < arr[x].size(); i++)
+    {
+        int y = arr[x][i];
+        dfs(y);
+    }
+}
+
+int main()
+{
+    arr[1].push_back(2);
+    arr[2].push_back(1);
+
+    arr[1].push_back(3);
+    arr[3].push_back(1);
+
+    arr[2].push_back(3);
+    arr[3].push_back(2);
+
+    arr[2].push_back(4);
+    arr[4].push_back(2);
+
+    arr[2].push_back(5);
+    arr[5].push_back(2);
+
+    arr[3].push_back(6);
+    arr[6].push_back(3);
+
+    arr[3].push_back(7);
+    arr[7].push_back(3);
+
+    arr[4].push_back(5);
+    arr[5].push_back(4);
+
+    arr[6].push_back(7);
+    arr[7].push_back(6);
+
+    dfs(1);
+
+    return 0;
+}
